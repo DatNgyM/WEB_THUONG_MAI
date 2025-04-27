bb77e1f672cfdd9c30b58d062914895d98f3187d
DatNgyM <150867973+DatNgyM@users.noreply.github.com>
2025-04-27 20:57:17 +0700

    admincheckpage

---
+++ JS/adminCheck.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    const role = localStorage.getItem('role');
+  
+    if (isLoggedIn && role === 'admin') {
+      setTimeout(() => {
+        if (confirm("Bạn đang đăng nhập bằng tài khoản admin.\nBạn muốn truy cập trang quản trị không?")) {
+          window.location.href = '/Page/admin/index.html';
+        }
+      }, 350); // ms 
+    }
+  });
+  
